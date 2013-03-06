65c5c3f4b8b17349e840588142aaf4f24e611b8d
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-06 14:57:33 +0100

    add package metadata

--- setup.py
+++ setup.py
@@ -11,12 +11,23 @@ setup(name='collective.js.bootstrap',
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
-        ],
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+      ],
       keywords='plone',
       author='JeanMichel aka toutpt',
       author_email='toutpt@gmail.com',
-      url='https://github.com/collective/',
+      url='https://github.com/toutpt/collective.js.bootstrap',
       license='gpl',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.js'],
@@ -24,6 +35,7 @@ setup(name='collective.js.bootstrap',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'plone.app.jquery>=1.7.2',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
